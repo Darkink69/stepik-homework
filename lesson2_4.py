bf97689d56e7ee4528c5c78405349f071a797d0b
Darkink69 <78140134+Darkink69@users.noreply.github.com>
2021-04-26 13:18:08 +0700

    Add files via upload
    ниче не понятно

---
+++ lesson2_4.py
@@ -0,0 +1,43 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium import webdriver
+import time
+import math
+
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+
+
+try:
+    browser = webdriver.Chrome()
+
+    browser.get("http://suninjuly.github.io/explicit_wait2.html")
+
+    # говорим Selenium проверять в течение 5 секунд, пока кнопка не станет кликабельной
+    price = WebDriverWait(browser, 12).until(
+        EC.text_to_be_present_in_element((By.ID, "price"), "$100"))
+
+    button = WebDriverWait(browser, 5).until(
+        EC.element_to_be_clickable((By.ID, "book"))
+    )
+    button.click()
+    
+    x_element = browser.find_element_by_css_selector('#input_value').text
+    print(x_element)
+    x = int(x_element)
+    y = calc(x)
+
+    browser.execute_script("window.scrollBy(0, 10);")
+    input1 = browser.find_element_by_css_selector('#answer').send_keys(y)
+    button = browser.find_element_by_css_selector("#solve").click()
+    
+    #print(price)
+
+
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
